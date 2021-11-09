99c3804929aea60c4cafbde2c62320dc5db38df0
Watthanatham Kruram <61160045@go.buu.ac.th>
2021-11-10 00:24:53 +0700

    test receipt v1

--- Project/src/main/java/model/Receipt.java
+++ Project/src/main/java/model/Receipt.java
@@ -24,10 +24,67 @@ public class Receipt {
         this.created = created;
         this.seller = seller;
         this.customer = customer;
+        receiptDetail = new ArrayList<>();
     }
 
     public Receipt(User seller, Customer customer) {
         this(-1, null, seller, customer);
+    }
+    
+    public void addReceiptDetail(int id, Product product , int amount, double price) {
+        receiptDetail.add(new ReceiptDetail(id, product, amount, price, this));
+    }
+    
+    public void addReceiptDetail(Product product , int amount, double price) {
+        addReceiptDetail(-1, product, amount, price);
+    }
+    
+    public double getTotal() {
+        double total = 0;
+        for(ReceiptDetail r : receiptDetail) {
+            total = total + r.getTotal();
+        }
+        return total;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public ArrayList<ReceiptDetail> getReceiptDetail() {
+        return receiptDetail;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setReceiptDetail(ArrayList<ReceiptDetail> receiptDetail) {
+        this.receiptDetail = receiptDetail;
     }
     
     
--- Project/src/main/java/model/ReceiptDetail.java
+++ Project/src/main/java/model/ReceiptDetail.java
@@ -10,5 +10,66 @@ package model;
  * @author watan
  */
 public class ReceiptDetail {
+    private int id;
+    private Product product;
+    private int amount;
+    private double price;
+    private Receipt receipt;
+
+    public ReceiptDetail(int id, Product product, int amount, double price, Receipt receipt) {
+        this.id = id;
+        this.product = product;
+        this.amount = amount;
+        this.price = price;
+        this.receipt = receipt;
+    }
+
+    public ReceiptDetail(Product product, int amount, double price, Receipt receipt) {
+        this(-1,product,amount, price, receipt);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Receipt getReceipt() {
+        return receipt;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setReceipt(Receipt receipt) {
+        this.receipt = receipt;
+    }
+    public double getTotal() {
+        return amount * price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
     
 }
